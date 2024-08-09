179312415cb731719bcb3be067cfed5f0c0a6384
shinkuan <shinkuan318@gmail.com>
2024-08-09 09:04:49 -0400

    virtual functions

--- inc/image.h
+++ inc/image.h
@@ -14,12 +14,12 @@ protected:
 public:
     Image(int w, int h);
     ~Image();
-    virtual bool load_image(string filename);
-    virtual void dump_image(string filename);
-    virtual void display_x_server();
-    virtual void display_ascii();
-    virtual void display_cmd();
-    virtual void resize(int w, int h);
+    virtual bool load_image(string filename) = 0;
+    virtual void dump_image(string filename) = 0;
+    virtual void display_x_server() = 0;
+    virtual void display_ascii() = 0;
+    virtual void display_cmd() = 0;
+    virtual void resize(int w, int h) = 0;
     int get_width();
     int get_height();
 
