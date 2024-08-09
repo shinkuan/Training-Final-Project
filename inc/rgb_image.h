84c1b9ce94e68f7ebfbc5afc3f90854a333c4bf3
shinkuan <shinkuan318@gmail.com>
2024-08-09 02:56:27 -0400

    refactor: Update GrayImage and RGBImage classes

--- inc/rgb_image.h
+++ inc/rgb_image.h
@@ -5,8 +5,17 @@
 
 class RGBImage : public Image{
 private:
+    int ***pixels;
 
 public:
+    RGBImage();
+    RGBImage(int w, int h, int ***p);
+    ~RGBImage();
+    bool load_image(string filename);
+    bool dump_image(string filename);
+    void display_x_server();
+    void display_ascii();
+    void display_cmd();
 
 };
 
