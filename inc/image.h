6e55329c4a5ec7bb4a8fba23752e4d2d2d39c323
shinkuan <shinkuan318@gmail.com>
2024-08-09 02:52:53 -0400

    refactor: Update Image class

--- inc/image.h
+++ inc/image.h
@@ -1,13 +1,26 @@
 #ifndef _IMAGE_H_
 #define _IMAGE_H_
 
+#include "data_loader.h"
+
 using namespace std;
 
 class Image{
 protected:
-
+    int width;
+    int height;
+    static Data_Loader loader; 
 
 public:
+    Image();
+    ~Image();
+    virtual bool load_image(string filename);
+    virtual bool dump_image(string filename);
+    virtual void display_x_server();
+    virtual void display_ascii();
+    virtual void display_cmd();
+    int get_width();
+    int get_height();
 
 };
 
